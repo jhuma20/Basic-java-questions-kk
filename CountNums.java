d1d2d4fa1f2066130d0cd94af0c3496282d2edf4
jhuma <135812485+jhuma20@users.noreply.github.com>
2024-04-01 19:06:34 +0530

    Add files via upload

---
+++ CountNums.java
@@ -0,0 +1,19 @@
+public class CountNums {
+    public static void main(String[] args) {
+        {
+            int n=433365;
+
+            int count =0;
+            while(n>0)
+            {
+                int rem=n%10;
+                if(rem==3)
+                {
+                    count++;
+                }
+                n=n/10;
+            }
+            System.out.println(count);
+        }
+    }
+}
---
+++ EvenOrOdd.java
@@ -0,0 +1,8 @@
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        {
+            Scanner sc= new Scanner(System.in)
+            int a = 
+        }
+    }
+}
