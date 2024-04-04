dacd2ab2cd0e815c36ad5fc756af841aee492c5f
jhuma <135812485+jhuma20@users.noreply.github.com>
2024-04-04 18:23:38 +0530

    Add files via upload

---
+++ Salary.java
@@ -0,0 +1,15 @@
+public class Salary {
+    public static void main(String[] args) {
+        {
+            int Salary = 25400;
+            if(Salary > 10000)
+            {
+         Salary = Salary + 2000;
+            }
+            else{
+                Salary = Salary + 1000;
+            }
+            System.out.println(Salary);
+            }
+        }
+    } 
