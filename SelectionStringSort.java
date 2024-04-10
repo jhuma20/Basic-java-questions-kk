0d38129f2471059a4cfcb32ce5e3f9313a36376f
jhuma <135812485+jhuma20@users.noreply.github.com>
2024-04-10 18:38:06 +0530

    Add files via upload

---
+++ SelectionStringSort.java
@@ -0,0 +1,26 @@
+public class SelectionStringSort {
+    public static void main(String[] args)
+    {
+         String[] a={"deepak","ram","sham","rohit","rishi"};
+         int min;
+         String temp;
+         for(int i=0;i<a.length;i++)
+         {
+            min=i;
+            for(int j=i+1; j<a.length;j++)
+            {
+                if(a[j].compareTo(a[min])<0)
+                {
+                    min=j;
+                }
+            }
+            temp=a[i];
+            a[i]=a[min];
+            a[min]=temp;
+         }
+         for(int i=0;i<a.length;i++)
+         {
+            System.out.print(a[i]+" ");
+         }
+    }
+}
